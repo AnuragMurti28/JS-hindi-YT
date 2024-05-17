2c2f2f57a7a6827256bdf94565179d3c165bc82a
Anurag Murti <81506371+AnuragMurti28@users.noreply.github.com>
2024-05-17 20:25:57 +0000

    03 completed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = 33;
+let score2 = "33";
+let score3 = "34abc"
+
+
+console.log(typeof score)
+console.log(typeof (score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+let valueNumber2 = Number(score2);
+console.log(typeof valueNumber2);
+let valueNumber3 = Number(score3);
+console.log(valueNumber3);// "34abc" will convert to NaN
+console.log(typeof valueNumber3); // NaN is of class Number
+
+// true => 1 false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn) // convert isLoggedIn =1 to boolean true
+console.log(booleanIsLoggedIn);
+//***********************************Operations****************************************** */
+
+let value = 3;
+let negValue = -value;
+
+console.log(2+4);
+console.log("2"+4); String + Number
+console.log(4+"2"+3);
+console.log(2+3+"4");
+console.log("4"+2+3);
+
+let string1 = "Anurag"
+let string2 = "Murti"
+
+console.log(true)
+
